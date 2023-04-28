eeb9f97d16ecba9e6f662e277a098098dad7facd
dynby <dyominyuri@gmail.com>
2023-04-28 22:46:57 +0300

    feat: add keyboard in dom

---
+++ index.js
@@ -0,0 +1,21 @@
+const codeArr = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ArrowUp', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'ControlLeft', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const keyArr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
+document.onkeydown = function (event) {
+  console.log(event);
+  keyArr.push(event.key);
+  console.log(keyArr);
+};
+
+const div = document.createElement('div');
+div.className = 'keyboard';
+document.body.append(div);
+
+function init() {
+  let out = '';
+  for (let i = 0; i < keyArr.length; i += 1) {
+    out += `<div class = "k-key" id = '${keyArr[i]}'>${keyArr[i]}</div>`;
+  }
+  document.querySelector('.keyboard').innerHTML = out;
+}
+
+init();
