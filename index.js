972c2742c98290a17240393e613a9bd23d386cbd
dynby <dyominyuri@gmail.com>
2023-05-01 12:37:31 +0300

    feat: add uppercase after keydown caps

--- index.js
+++ index.js
@@ -4,47 +4,80 @@ const codeArr = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 
   'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
   'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 const keyArr = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-  'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del',
-  'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter',
-  'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '↑', 'Shift',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift',
   'Ctrl', 'Win', 'Alt', 'Space', 'Alt', '←', '↓', '→', 'Ctrl'];
 const div = document.createElement('div');
 const textarea = document.createElement('textarea');
-document.onkeydown = function (event) {
-  console.log(event);
-  keyArr.push(event.key);
-  console.log(keyArr);
-};
-function stopDefAction(evt) {
-  evt.preventDefault();
-}
 textarea.className = 'textarea';
 document.body.append(textarea);
 div.className = 'keyboard';
 document.body.append(div);
 function setFocus() {
   document.querySelector('.textarea').focus();
-  document.querySelector('.textarea', stopDefAction);
 }
 document.body.addEventListener('onload', setFocus());
 function init() {
   let out = '';
   for (let i = 0; i < keyArr.length; i += 1) {
-    out += `<div class = "k-key" id = '${keyArr[i]}' data = '${codeArr[i]}'>${keyArr[i]}</div>`;
+    if ((keyArr[i] === 'q') || (keyArr[i] === 'w') || (keyArr[i] === 'e') || (keyArr[i] === 'r') || (keyArr[i] === 't')
+    || (keyArr[i] === 'y') || (keyArr[i] === 'u') || (keyArr[i] === 'i') || (keyArr[i] === 'o') || (keyArr[i] === 'p')
+    || (keyArr[i] === 'a') || (keyArr[i] === 's') || (keyArr[i] === 'd') || (keyArr[i] === 'f') || (keyArr[i] === 'g')
+    || (keyArr[i] === 'h') || (keyArr[i] === 'j') || (keyArr[i] === 'k') || (keyArr[i] === 'l') || (keyArr[i] === 'z')
+    || (keyArr[i] === 'x') || (keyArr[i] === 'c') || (keyArr[i] === 'v') || (keyArr[i] === 'b') || (keyArr[i] === 'n') || (keyArr[i] === 'm')) {
+      out += `<div class = "k-key-alphabet" id = '${keyArr[i]}' data = '${codeArr[i]}'>${keyArr[i]}</div>`;
+      document.querySelector('.keyboard').innerHTML = out;
+    } else {
+      out += `<div class = "k-key" id = '${keyArr[i]}' data = '${codeArr[i]}'>${keyArr[i]}</div>`;
+      document.querySelector('.keyboard').innerHTML = out;
+    }
   }
-  document.querySelector('.keyboard').innerHTML = out;
 }
 
 init();
 
+// caps
+const alphabet = document.querySelectorAll('.k-key-alphabet');
+
+let checkCaps = false;
+const caps = function () {
+  if (checkCaps === false) {
+    for (let i = 0; i < alphabet.length; i += 1) {
+      let getAlpha = alphabet[i].textContent;
+      const up = getAlpha.toUpperCase();
+      alphabet[i].textContent = up;
+    }
+    checkCaps = true;
+  } else {
+    for (let i = 0; i < alphabet.length; i += 1) {
+      let getAlpha = alphabet[i].textContent;
+      const low = getAlpha.toLowerCase();
+      alphabet[i].textContent = low;
+    }
+    checkCaps = false;
+  }
+};
+
 document.onkeydown = function (event) {
-  stopDefAction();
-  document.querySelector('.k-key[data="' + event.code + '"]').classList.add('active');
-  if (event.key !== 'Tab' || event.key !== 'Backspace') {
-    textarea.innerHTML += event.key;
+  document.querySelector(`.k-key[data="${event.code}"]`).classList.add('active');
+  if (event.code === 'CapsLock') {
+    caps();
   }
 };
 
 document.onkeyup = function (event) {
-  document.querySelector('.k-key[data="' + event.code + '"]').classList.remove('active');
+  document.querySelector(`.k-key[data="${event.code}"]`).classList.remove('active');
 };
+
+document.querySelectorAll('.k-key').forEach((el) => {
+  el.addEventListener('mousedown', () => {
+    el.classList.add('active');
+  });
+});
+
+document.querySelectorAll('.k-key').forEach((el) => {
+  el.addEventListener('mouseup', () => {
+    el.classList.remove('active', 200);
+  });
+});
